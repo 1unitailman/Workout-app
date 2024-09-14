ae9ed3e46cefc8738d6088defbf7d1397fd339b2
1unitailman <mrsaenko1@gmail.com>
2024-09-14 14:33:10 +0300

    Home page created

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,20 +1,40 @@
+import { useLocation, useNavigate } from 'react-router-dom';
 import { useAuth } from '../../../hooks/useAuth'
 import { Hamburger } from '../hamburger/Hamburger';
 import styles from './Header.module.scss'
 
 import { FaArrowLeft } from "react-icons/fa6";
+import { CgProfile } from "react-icons/cg";
 
-const Header = () => {
-  const {isAuth} = useAuth()
+const Header = ({ backLink = '' }) => {
+	const { pathname } = useLocation()
+	const navigate = useNavigate()
 
-  return (
-    <header className={styles.header}>
-      <button onClick={() => {}}>
-        <FaArrowLeft fontSize={25}/>
-      </button>
-      <Hamburger />
-    </header>
-  )
+	const { isAuth } = useAuth()
+
+	return (
+		<header className={styles.header}>
+			{pathname !== '/' ? (
+				<button
+					onClick={() => {
+						navigate(backLink)
+					}}
+				>
+					<FaArrowLeft fill='#fff' fontSize={25} />
+				</button>
+			) : (
+				<button
+					onClick={() => {
+						navigate('/profile')
+					}}
+				>
+					<CgProfile fill='#fff' fontSize={25} />
+				</button>
+			)}
+			<Hamburger />
+		</header>
+	)
 }
 
 export default Header
+
--- src/components/screens/home/Home.jsx
+++ src/components/screens/home/Home.jsx
@@ -1,10 +1,25 @@
-import Header from "../../layout/header/Header"
+import { useNavigate, useNavigation } from "react-router-dom"
+
 import Layout from "../../layout/Layout"
 
+import Button from "../../ui/button/Button"
+
+import styles from './Home.module.scss'
+
+import { useAuth } from "../../../hooks/useAuth"
+
 function Home() {
+	const {isAuth} = useAuth()
+
+	const navigate = useNavigate()
+
 	return (
-		<Layout>
-			<Header />
+		<Layout bgImage='/public/home-bg.jpg'>
+			<Button 
+				clickHandler={() => navigate(!isAuth ? '/new-workout' : '/auth')}>
+					{isAuth ? 'New' : 'Log in'}
+			</Button>
+			<h1 className = {styles.heading}>Exercises for shoulders</h1>
 		</Layout>
 	)
 }
